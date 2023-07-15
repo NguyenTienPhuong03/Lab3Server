9c9a93c2825b29f23eefc383f2876268c339afb4
Tienphuongnguyen03 <91860173+NguyenTienPhuong03@users.noreply.github.com>
2023-07-15 22:03:02 +0700

    Update demo-readfilebonus.js

--- demo-readfilebonus.js
+++ demo-readfilebonus.js
@@ -1,4 +1,4 @@
 var fs = require('fs');
 var data = "\n Trương Thu Huyền ";
-fs.appendFileSync('thongtin.txt', data,'utf8');
+fs.appendFileSync('mssv.txt', data,'utf8');
 console.log('Data is Appended to file success');
